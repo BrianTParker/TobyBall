510bca1209111b8568842ed02d1bd27c516dabfb
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-07 17:53:57 -0400

    - created class Health.java; used to control a player's health

---
+++ TobyBall/src/com/game/TobyBall/Health.java
@@ -0,0 +1,12 @@
+package com.game.TobyBall;
+
+import project_sanity.essence.AEssence;
+
+/**
+ * Health system for TobyBall. 
+ * 
+ * @author nicholas
+ */
+public class Health extends AEssence {
+    
+}
