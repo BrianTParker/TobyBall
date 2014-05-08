ac32a1261c03929cd9046630c1f32bbab433c7fe
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-07 23:05:58 -0400

    - Health.java now extends AEssence.java - Health.java now has a min/max range - Health.java constructor now accepts min/max as parameters - Health.java now overrides getValue parent method

--- TobyBall/src/com/game/TobyBall/Health.java
+++ TobyBall/src/com/game/TobyBall/Health.java
@@ -1,6 +1,7 @@
 package com.game.TobyBall;
 
 import project_sanity.essence.AEssence;
+import project_sanity.range.Range;
 
 /**
  * Health system for TobyBall. 
@@ -9,4 +10,15 @@ import project_sanity.essence.AEssence;
  */
 public class Health extends AEssence {
     
+    private Range m_range;
+
+    public Health( int inMin, int inMax ) {
+        
+        m_range = new Range( inMin, inMax );
+    }
+
+    @Override
+    public int getValue() {
+        return m_range.getMAX() - super.getValue();
+    }
 }
